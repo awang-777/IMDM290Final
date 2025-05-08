5e87ae81da2b423e40e466b0a65166b81bd33e4a
MaxAntDav <maxantdavenport@gmail.com>
2025-05-07 23:22:33 -0400

    Update MapSelector.cs

--- Assets/Script/PlayerSpawns/MapSelector.cs
+++ Assets/Script/PlayerSpawns/MapSelector.cs
@@ -3,14 +3,13 @@ using UnityEngine.SceneManagement;
 
 public class MapTrigger : MonoBehaviour
 {
-    public int mapIndex = 1; // Set this in the inspector: 1, 2, or 3
-
+    public int mapIndex = 1;
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             PlayerPrefs.SetInt("SelectedMap", mapIndex);
-            SceneManager.LoadScene("GameScene"); // Replace with your actual game scene name
+            SceneManager.LoadScene("GameScene");
         }
     }
 }
